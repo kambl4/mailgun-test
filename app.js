b1d3b27d01bf91539aacbd078697264aa998c446
Pavel Tarasenko <goo.kambl4@gmail.com>
2020-02-10 12:00:21 +0300

    fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let api = 'key-XXXXXXXXXXXXXXXXXXXXXX'; // change this!!!
 let domain = 'https://api.eu.mailgun.net/v3/develop.ulpanbot.ru';
 
-mailgun = require('mailgun-js')({ apiKey: api, domain });
+const mailgun = require('mailgun-js')({ apiKey: api, domain });
 
 const data = {
   from: 'postmaster@develop.ulpanbot.ru',
