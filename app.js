f385b0d405fe8c95b6491ec5b1e6d4dfb879ecd1
Pavel Tarasenko <goo.kambl4@gmail.com>
2020-02-10 12:03:31 +0300

    cleanup

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let api = 'key-XXXXXXXXXXXXXXXXXXXXXX'; // change this!!!
+let apiKey = 'key-XXXXXXXXXXXXXXXXXXXXXX'; // change this!!!
 let domain = 'https://api.eu.mailgun.net/v3/develop.ulpanbot.ru';
 
-const mailgun = require('mailgun-js')({ apiKey: api, domain });
+const mailgun = require('mailgun-js')({ apiKey, domain });
 
-const data = {
-  from: 'postmaster@develop.ulpanbot.ru',
+let data = {
+  from: 'Ulpanika <postmaster@develop.ulpanbot.ru>',
   to: 'goo.kambl4@gmail.com',
   subject: 'Test email subject',
   text: 'Test email text'
